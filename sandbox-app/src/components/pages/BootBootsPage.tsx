22182dc1e3b9dfaac756a64d5fac088dc00395fe
Martin Harris <1488244+nakomis@users.noreply.github.com>
2026-01-30 15:40:26 +0000

    Enlarges cat labeling images

--- sandbox-app/src/components/pages/BootBootsPage.tsx
+++ sandbox-app/src/components/pages/BootBootsPage.tsx
@@ -29,11 +29,11 @@ const BootBootsPage = (props: BootBootProps) => {
                 return (
                     <div style={{
                         backgroundColor: '#1f2329',
-                        minHeight: '400px',
+                        height: 'calc(100vh - 120px)',
                         display: 'flex',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        width: '99%',
+                        width: '100%',
                         color: 'white'
                     }}>
                         <p>We're all out of Kitties!</p>
@@ -43,11 +43,11 @@ const BootBootsPage = (props: BootBootProps) => {
                 return (
                     <div style={{
                         backgroundColor: '#1f2329',
-                        minHeight: '400px',
+                        height: 'calc(100vh - 120px)',
                         display: 'flex',
                         alignItems: 'center',
                         justifyContent: 'center',
-                        width: '99%',
+                        width: '100%',
                         color: 'white'
                     }}>
                         <p>Loading the next Kitty...</p>
@@ -57,110 +57,122 @@ const BootBootsPage = (props: BootBootProps) => {
         }
 
         const imgdiv = (
-            <img className="img-fluid"
-                id="cat-image"
-                src={`${catPicture}`}
-                alt="Cat"
-                style={{
-                    width: "99%",
-                    height: "auto",
-                    maxHeight: "400px",
-                    objectFit: "contain"
-                }}
-                onLoad={() => {
-                    setTimeout(function () {
-                        document.getElementById("outerdiv")!.style.width = "99%";
+            <div style={{
+                flex: 1,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: 0,
+                overflow: 'hidden'
+            }}>
+                <img className="img-fluid"
+                    id="cat-image"
+                    src={`${catPicture}`}
+                    alt="Cat"
+                    style={{
+                        maxWidth: "100%",
+                        maxHeight: "100%",
+                        objectFit: "contain"
+                    }}
+                    onLoad={() => {
                         setTimeout(function () {
-                            document.getElementById("outerdiv")!.style.width = "100%";
+                            document.getElementById("outerdiv")!.style.width = "99%";
+                            setTimeout(function () {
+                                document.getElementById("outerdiv")!.style.width = "100%";
+                            }, 50);
                         }, 50);
-                    }, 50);
-                }}
-            />
+                    }}
+                />
+            </div>
         )
         return (
-            <div>
+            <div style={{ height: 'calc(100vh - 120px)', display: 'flex', flexDirection: 'column' }}>
                 <KeyPressComponent {...{ onKeyUp: handleKeyUp }} />
-                <h3 style={{ marginTop: 7 }}>Giving Boots the boot. {catadataRecords ? catadataRecords.length === 1 ? "Only 1 image " : `Only ${catadataRecords.length} images ` : 0} left to go!</h3>
+                <h3 style={{ marginTop: 7, flexShrink: 0 }}>Giving Boots the boot. {catadataRecords ? catadataRecords.length === 1 ? "Only 1 image " : `Only ${catadataRecords.length} images ` : 0} left to go!</h3>
                 <div id="outerdiv" style={{
                     backgroundColor: '#1f2329',
                     padding: '20px',
-                    width: '99%',
-                    height: '80vh',
+                    width: '100%',
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minHeight: 0
                 }}>
                     {imgdiv}
-                    <button
-                        className="btn btn-primary"
-                        title="Mu (M)"
-                        onClick={() => {
-                            clickCat("Mu")
-                        }}
-                        style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
-                    >
-                        <img src={mu} alt="Mu" style={{ width: '100%', height: '100%' }} />
-                    </button>
-                    <button
-                        className="btn btn-primary"
-                        title="Tau (T)"
-                        onClick={() => {
-                            clickCat("Tau");
-                        }}
-                        style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
-                    >
-                        <img src={tau} alt="Tau" style={{ width: '100%', height: '100%' }} />
-                    </button>
-                    <button
-                        className="btn btn-primary"
-                        title="Chi (C)"
-                        onClick={() => {
-                            clickCat("Chi");
-                        }}
-                        style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
-                    >
-                        <img src={chi} alt="Chi" style={{ width: '100%', height: '100%' }} />
-                    </button>
-                    <button
-                        className="btn btn-primary"
-                        title="Kappa (K)"
-                        onClick={() => {
-                            clickCat("Kappa");
-                        }}
-                        style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
-                    >
-                        <img src={kappa} alt="Kappa" style={{ width: '100%', height: '100%' }} />
-                    </button>
-                    <br></br>
-                    <button
-                        className="btn btn-primary"
-                        title="Boots (B)"
-                        onClick={() => {
-                            clickCat("Boots");
-                        }}
-                        style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
-                    >
-                        <img src={boots} alt="Boots" style={{ width: '100%', height: '100%' }} />
-                    </button>
-                    <button
-                        className="btn btn-primary"
-                        title="Wolf (W)"
-                        onClick={() => {
-                            clickCat("Wolf");
-                        }}
-                        style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
-                    >
-                        <img src={wolf} alt="Wolf" style={{ width: '100%', height: '100%' }} />
-                    </button>
-                    <button
-                        className="btn btn-primary"
-                        title="No Cat (N)"
-                        onClick={() => {
-                            clickCat("NoCat");
-                        }}
-                        style={{ width: '200px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                        </svg>
-                    </button>
+                    <div style={{ flexShrink: 0, paddingTop: '10px' }}>
+                        <button
+                            className="btn btn-primary"
+                            title="Mu (M)"
+                            onClick={() => {
+                                clickCat("Mu")
+                            }}
+                            style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
+                        >
+                            <img src={mu} alt="Mu" style={{ width: '100%', height: '100%' }} />
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            title="Tau (T)"
+                            onClick={() => {
+                                clickCat("Tau");
+                            }}
+                            style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
+                        >
+                            <img src={tau} alt="Tau" style={{ width: '100%', height: '100%' }} />
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            title="Chi (C)"
+                            onClick={() => {
+                                clickCat("Chi");
+                            }}
+                            style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
+                        >
+                            <img src={chi} alt="Chi" style={{ width: '100%', height: '100%' }} />
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            title="Kappa (K)"
+                            onClick={() => {
+                                clickCat("Kappa");
+                            }}
+                            style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
+                        >
+                            <img src={kappa} alt="Kappa" style={{ width: '100%', height: '100%' }} />
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            title="Boots (B)"
+                            onClick={() => {
+                                clickCat("Boots");
+                            }}
+                            style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
+                        >
+                            <img src={boots} alt="Boots" style={{ width: '100%', height: '100%' }} />
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            title="Wolf (W)"
+                            onClick={() => {
+                                clickCat("Wolf");
+                            }}
+                            style={{ width: '100px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
+                        >
+                            <img src={wolf} alt="Wolf" style={{ width: '100%', height: '100%' }} />
+                        </button>
+                        <button
+                            className="btn btn-primary"
+                            title="No Cat (N)"
+                            onClick={() => {
+                                clickCat("NoCat");
+                            }}
+                            style={{ width: '200px', height: '100px', backgroundColor: '#3b4048ff', border: 'none', padding: 0, margin: 5 }}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
         )
